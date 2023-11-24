63caa1d60e6a6850affad82b4ad3b7f261ba6b4a
xiaoming <2014500726@smail.xtu.edu.cn>
2023-11-24 22:36:49 +0800

    Fix canFetchMore error
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- src/qcustomfilesystemmodel.cpp
+++ src/qcustomfilesystemmodel.cpp
@@ -215,9 +215,11 @@ bool QCustomFileSystemModel::canFetchMore(const QModelIndex &parent) const {
     if (!parent.isValid())
         return false;
     QCustomFileSystemItem *parentItem = static_cast<QCustomFileSystemItem*>(parent.internalPointer());
-    QDir parentDir(parentItem->data().toString());
-    QStringList entries = parentDir.entryList(QDir::NoDotAndDotDot | QDir::AllEntries | QDir::Hidden);
-    return entries.count() > 0;
+    if(parentItem->childCount() != 1)
+        return false;
+    if(parentItem->child(0)->data().toString() != "")
+        return false;
+    return true;
 }
 
 QModelIndex QCustomFileSystemModel::setRootPath(const QString &path) {
