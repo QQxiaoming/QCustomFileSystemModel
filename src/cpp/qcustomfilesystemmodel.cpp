127140619b1b734994c90a532d1cad42212f3c7a
xiaoming <2014500726@smail.xtu.edu.cn>
2023-11-27 11:39:35 +0800

    Update license headers in QCustomFileSystemModel files
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- src/cpp/qcustomfilesystemmodel.cpp
+++ src/cpp/qcustomfilesystemmodel.cpp
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the https://github.com/QQxiaoming/QCustomFileSystemModel.git
+ * project.
+ *
+ * Copyright (C) 2023 Quard <2014500726@smail.xtu.edu.cn>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */ 
 #include <QDir>
 #include <QFileInfo>
 #include <QIcon>
--- src/python/qcustomfilesystemmodel.py
+++ src/python/qcustomfilesystemmodel.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+# This file is part of the https://github.com/QQxiaoming/QCustomFileSystemModel.git
+# project.
+# 
+# Copyright (C) 2023 Quard <2014500726@smail.xtu.edu.cn>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 from PySide6.QtCore import (QAbstractItemModel, QFileInfo, QDir, QModelIndex, Qt, Signal, Slot)
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
